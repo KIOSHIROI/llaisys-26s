2cb36a5037258a54a746ecad6cbeb63e0e5fe939
kioshiro <poetyuqiubai@163.com>
2026-08-11 17:24:37 +0800

    argmax_cpu

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,19 @@
 #include "op.hpp"
+#include "cpu/argmax_cpu.hpp"
 
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    llaisys::core::context().setDevice(vals->deviceType(), vals->deviceId());
+
+    switch (vals->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::argmax(max_idx->data(), max_val->data(), vals->data(), vals->dtype(), vals->numel());
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        return nvidia::argmax(max_idx->data(), max_val->data(), vals->data(), vals->dtype(), vals->numel());
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
